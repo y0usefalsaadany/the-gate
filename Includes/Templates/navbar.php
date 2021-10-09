8de5b7f551a738d6498290578e320d90f2f673c7
Yousef Alsaadany <58972656+y0usefalsaadany@users.noreply.github.com>
2021-10-09 19:37:23 +0200

    Add files via upload

---
+++ Includes/Templates/navbar.php
@@ -0,0 +1,11 @@
+<?php include "header.php";?>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+   <ul class="navbar">
+		 <a class="navbar-brand" href="index.php">The Gate</a>
+	</ul>
+        <ul class="navbar">
+        <a class="navbar-brand" href="manual.php">Manual</a>
+	<a class="navbar-brand" href="automatic.php">Automatic</a>
+          </ul>
+    </nav>
+    
